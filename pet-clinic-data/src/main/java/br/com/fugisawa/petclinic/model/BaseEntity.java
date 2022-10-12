253d1eedeec8c88d4eee5af5b36537a90767c9b9
Lucas Fugisawa <lucasfugisawa@gmail.com>
2022-10-12 09:11:58 -0300

    Adding id (Long) to model objects. Closes #9.

---
+++ pet-clinic-data/src/main/java/br/com/fugisawa/petclinic/model/BaseEntity.java
@@ -0,0 +1,17 @@
+package br.com.fugisawa.petclinic.model;
+
+import javax.persistence.Id;
+import java.io.Serializable;
+
+public class BaseEntity implements Serializable {
+
+    private Long id;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+}
--- pet-clinic-data/src/main/java/br/com/fugisawa/petclinic/model/PetType.java
+++ pet-clinic-data/src/main/java/br/com/fugisawa/petclinic/model/PetType.java
@@ -1,6 +1,6 @@
 package br.com.fugisawa.petclinic.model;
 
-public class PetType {
+public class PetType extends BaseEntity {
     private String name;
 
     public String getName() {
