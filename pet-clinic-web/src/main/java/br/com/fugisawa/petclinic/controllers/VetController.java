51392c416f49347557cec85a49e793d7cc9dc6a7
Lucas Fugisawa <lucasfugisawa@gmail.com>
2022-10-13 11:20:52 -0300

    List all Vets on Vet index page. Closes #14.

--- pet-clinic-web/src/main/java/br/com/fugisawa/petclinic/controllers/VetController.java
+++ pet-clinic-web/src/main/java/br/com/fugisawa/petclinic/controllers/VetController.java
@@ -1,14 +1,23 @@
 package br.com.fugisawa.petclinic.controllers;
 
+import br.com.fugisawa.petclinic.services.VetService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @RequestMapping("/vets")
 @Controller
 public class VetController {
 
+    private final VetService vetService;
+
+    public VetController(VetService vetService) {
+        this.vetService = vetService;
+    }
+
     @RequestMapping({"", "/", "/index", "/index.html"})
-    public String listVets() {
+    public String listVets(Model model) {
+        model.addAttribute("vets", this.vetService.findAll());
         return "vets/index";
     }
 }
