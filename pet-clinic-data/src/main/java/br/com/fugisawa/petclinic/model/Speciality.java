c6e5c9e873104a09d5cf7cc33584d6731cefaff8
Lucas Fugisawa <lucasfugisawa@gmail.com>
2022-10-14 16:03:28 -0300

    Create PetType, Pet and Visit entities. Closes #25.

---
+++ pet-clinic-data/src/main/java/br/com/fugisawa/petclinic/model/Speciality.java
@@ -0,0 +1,14 @@
+package br.com.fugisawa.petclinic.model;
+
+public class Speciality extends BaseEntity {
+
+    private String description;
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+}
