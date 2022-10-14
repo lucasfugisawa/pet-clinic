a5e04afb1d5df4b09fde19894cc927bb8ac3a142
Lucas Fugisawa <lucasfugisawa@gmail.com>
2022-10-14 16:00:24 -0300

    Create PetType, Pet and Visit entities. Closes #25.

--- pet-clinic-data/src/main/java/br/com/fugisawa/petclinic/model/Pet.java
+++ pet-clinic-data/src/main/java/br/com/fugisawa/petclinic/model/Pet.java
@@ -3,6 +3,7 @@ package br.com.fugisawa.petclinic.model;
 import java.time.LocalDate;
 
 public class Pet extends BaseEntity {
+
     private PetType petType;
     private Owner owner;
     private LocalDate birthDate;
