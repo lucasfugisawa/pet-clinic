3724f21538228d44bb0ae695d52e723064dbae95
Lucas Fugisawa <lucasfugisawa@gmail.com>
2022-10-15 20:48:47 -0300

    Convert Owners to JPA entities. Closes #35.

--- pet-clinic-data/src/main/java/br/com/fugisawa/petclinic/model/Owner.java
+++ pet-clinic-data/src/main/java/br/com/fugisawa/petclinic/model/Owner.java
@@ -1,13 +1,23 @@
 package br.com.fugisawa.petclinic.model;
 
+import javax.persistence.*;
 import java.util.HashSet;
 import java.util.Set;
 
+@Entity
+@Table(name = "owners")
 public class Owner extends Person {
 
+    @Column(name = "address")
     private String address;
+
+    @Column(name = "city")
     private String city;
+
+    @Column(name = "telephone")
     private String telephone;
+
+    @OneToMany(cascade = CascadeType.ALL, mappedBy = "owner")
     private Set<Pet> pets = new HashSet<>();
 
     public String getAddress() {
@@ -40,5 +50,9 @@ public class Owner extends Person {
 
     public void setPets(Set<Pet> pets) {
         this.pets = pets;
+    }
+
+    public static void main(String[] args) {
+
     }
 }
--- pet-clinic-data/src/main/java/br/com/fugisawa/petclinic/model/Pet.java
+++ pet-clinic-data/src/main/java/br/com/fugisawa/petclinic/model/Pet.java
@@ -1,14 +1,25 @@
 package br.com.fugisawa.petclinic.model;
 
+import javax.persistence.*;
 import java.time.LocalDate;
 
+@Entity
+@Table(name = "pets")
 public class Pet extends BaseEntity {
 
+    @Column(name = "name")
     private String name;
 
+    @Column
+    @ManyToOne()
+    @JoinColumn(name = "type_id")
     private PetType petType;
 
+    @ManyToOne
+    @Column(name = "owner_id")
     private Owner owner;
+
+    @Column(name = "birth_date")
     private LocalDate birthDate;
 
     public String getName() {
